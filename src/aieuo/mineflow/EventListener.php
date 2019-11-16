0a0be6b53ae225ea6cc40274d9cf2681f0560df2
aieuo <35859665+aieuo@users.noreply.github.com>
2019-11-17 00:32:45 +0900

    トリガーに使用するイベントを選択できるように

--- src/aieuo/mineflow/EventListener.php
+++ src/aieuo/mineflow/EventListener.php
@@ -2,17 +2,40 @@
 
 namespace aieuo\mineflow;
 
-use aieuo\mineflow\trigger\TriggerManager;
-use aieuo\mineflow\ui\TriggerForm;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\Listener;
-use aieuo\mineflow\utils\Session;
-use pocketmine\event\EventPriority;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\plugin\MethodEventExecutor;
 use pocketmine\utils\Config;
+use pocketmine\plugin\MethodEventExecutor;
+use pocketmine\event\server\LowMemoryEvent;
+use pocketmine\event\player\PlayerToggleSprintEvent;
+use pocketmine\event\player\PlayerToggleSneakEvent;
+use pocketmine\event\player\PlayerToggleFlightEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerChangeSkinEvent;
+use pocketmine\event\player\PlayerBedEnterEvent;
+use pocketmine\event\level\LevelLoadEvent;
+use pocketmine\event\inventory\FurnaceBurnEvent;
+use pocketmine\event\inventory\CraftItemEvent;
+use pocketmine\event\entity\ProjectileHitEntityEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\EventPriority;
+use pocketmine\event\Event;
+use aieuo\mineflow\utils\Session;
+use aieuo\mineflow\ui\TriggerForm;
+use aieuo\mineflow\trigger\TriggerManager;
+use aieuo\mineflow\Main;
 
 class EventListener implements Listener {
 
@@ -21,6 +44,36 @@ class EventListener implements Listener {
 
     /** @var array */
     private $enabledEvents = [];
+
+    /** @var array */
+    private $eventMethods = [
+        "PlayerChatEvent" => "onChat",
+        "PlayerCommandPreprocessEvent" => "onCommandPreprocess",
+        "PlayerInteractEvent" => "onInteract",
+        "PlayerJoinEvent" => "onJoin",
+        "PlayerQuitEvent" => "onQuit",
+        "BlockBreakEvent" => "onBlockBreak",
+        "BlockPlaceEvent" => "onBlockPlace",
+        "SignChangeEvent" => "onSignChange",
+        "EntityDamageEvent" => "onEntityDamage",
+        // "EntityAttackEvent" => "onEntityAttack",
+        "PlayerToggleFlightEvent" => "onToggleFlight",
+        "PlayerDeathEvent" => "onDeath",
+        "EntityLevelChangeEvent" => "onLevelChange",
+        "CraftItemEvent" => "onCraftItem",
+        "PlayerDropItemEvent" => "onDropItem",
+        "FurnaceBurnEvent" => "onFurnaceBurn",
+        "LevelLoadEvent" => "onLevelLoad",
+        "PlayerBedEnterEvent" => "onBedEnter",
+        "PlayerChangeSkinEvent" => "onChangeSkin",
+        "PlayerExhaustEvent" => "onExhaust",
+        "PlayerItemConsumeEvent" => "onItemConsume",
+        "PlayerMoveEvent" => "onMove",
+        "PlayerToggleSneakEvent" => "onToggleSneak",
+        "PlayerToggleSprintEvent" => "onToggleSprint",
+        "LowMemoryEvent" => "onLowMemory",
+        "ProjectileHitEntityEvent" => "onProjectileHit",
+    ];
 
     public function __construct(Main $owner, Config $eventSettings) {
         $this->owner = $owner;
@@ -43,13 +96,15 @@ class EventListener implements Listener {
     }
 
     public function registerEvents() {
-        $enables = $this->enabledEvents;
-        $playerEvent = "pocketmine\\event\\player\\";
-        $blockEvent = "pocketmine\\event\\block\\";
+        $manager = TriggerManager::getManager(TriggerManager::TRIGGER_EVENT);
 
-        $this->registerEvent($playerEvent."PlayerJoinEvent", "onJoin");
-        $this->registerEvent($playerEvent."PlayerQuitEvent", "onQuit");
-        $this->registerEvent($playerEvent."PlayerInteractEvent", "onInteract");
+        $this->registerEvent(PlayerJoinEvent::class, "onJoin");
+        $this->registerEvent(PlayerQuitEvent::class, "onQuit");
+        $this->registerEvent(PlayerInteractEvent::class, "onInteract");
+
+        foreach ($this->enabledEvents as $event => $value) {
+            $this->registerEvent($manager->getEventPath($event), $this->eventMethods[$event]);
+        }
     }
 
     private function registerEvent(string $event, string $method) {
@@ -60,10 +115,14 @@ class EventListener implements Listener {
 
     public function onJoin(PlayerJoinEvent $event) {
         Session::createSession($event->getPlayer());
+
+        if (isset($this->enabledEvents["PlayerJoinEvent"])) $this->onEvent($event, "PlayerJoinEvent");
     }
 
     public function onQuit(PlayerQuitEvent $event) {
         Session::destroySession($event->getPlayer());
+
+        if (isset($this->enabledEvents["PlayerQuitEvent"])) $this->onEvent($event, "PlayerQuitEvent");
     }
 
     public function onInteract(PlayerInteractEvent $event) {
@@ -95,5 +154,78 @@ class EventListener implements Listener {
             $recipes = $manager->get($position);
             $recipes->executeAll($player);
         }
+
+        if (isset($this->enabledEvents["PlayerInteractEvent"])) $this->onEvent($event, "PlayerInteractEvent");
+    }
+
+    public function onEvent(Event $event, string $eventName): void {
+
+    }
+
+    public function onChat(PlayerChatEvent $event) {
+        $this->onEvent($event, "PlayerChatEvent");
+    }
+    public function onCommandPreprocess(PlayerCommandPreprocessEvent $event) {
+        $this->onEvent($event, "PlayerCommandPreprocessEvent");
+    }
+    public function onBlockBreak(BlockBreakEvent $event) {
+        $this->onEvent($event, "BlockBreakEvent");
+    }
+    public function onBlockPlace(BlockPlaceEvent $event) {
+        $this->onEvent($event, "BlockPlaceEvent");
+    }
+    public function onSignChange(SignChangeEvent $event) {
+        $this->onEvent($event, "SignChangeEvent");
+    }
+    public function onEntityDamage(EntityDamageEvent $event) {
+        $this->onEvent($event, "EntityDamageEvent");
+    }
+    public function onToggleFlight(PlayerToggleFlightEvent $event) {
+        $this->onEvent($event, "PlayerToggleFlightEvent");
+    }
+    public function onDeath(PlayerDeathEvent $event) {
+        $this->onEvent($event, "PlayerDeathEvent");
+    }
+    public function onLevelChange(EntityLevelChangeEvent $event) {
+        $this->onEvent($event, "EntityLevelChangeEvent");
+    }
+    public function onCraftItem(CraftItemEvent $event) {
+        $this->onEvent($event, "CraftItemEvent");
+    }
+    public function onDropItem(PlayerDropItemEvent $event) {
+        $this->onEvent($event, "PlayerDropItemEvent");
+    }
+    public function onFurnaceBurn(FurnaceBurnEvent $event) {
+        $this->onEvent($event, "FurnaceBurnEvent");
+    }
+    public function onLevelLoad(LevelLoadEvent $event) {
+        $this->onEvent($event, "LevelLoadEvent");
+    }
+    public function onBedEnter(PlayerBedEnterEvent $event) {
+        $this->onEvent($event, "PlayerBedEnterEvent");
+    }
+    public function onChangeSkin(PlayerChangeSkinEvent $event) {
+        $this->onEvent($event, "PlayerChangeSkinEvent");
+    }
+    public function onExhaust(PlayerExhaustEvent $event) {
+        $this->onEvent($event, "PlayerExhaustEvent");
+    }
+    public function onItemConsume(PlayerItemConsumeEvent $event) {
+        $this->onEvent($event, "PlayerItemConsumeEvent");
+    }
+    public function onMove(PlayerMoveEvent $event) {
+        $this->onEvent($event, "PlayerMoveEvent");
+    }
+    public function onToggleSneak(PlayerToggleSneakEvent $event) {
+        $this->onEvent($event, "PlayerToggleSneakEvent");
+    }
+    public function onToggleSprint(PlayerToggleSprintEvent $event) {
+        $this->onEvent($event, "PlayerToggleSprintEvent");
+    }
+    public function onLowMemory(LowMemoryEvent $event) {
+        $this->onEvent($event, "LowMemoryEvent");
+    }
+    public function onProjectileHit(ProjectileHitEntityEvent $event) {
+        $this->onEvent($event, "ProjectileHitEntityEvent");
     }
 }
--- src/aieuo/mineflow/trigger/EventTriggerManager.php
+++ src/aieuo/mineflow/trigger/EventTriggerManager.php
@@ -2,24 +2,62 @@
 
 namespace aieuo\mineflow\trigger;
 
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\entity\ProjectileHitEntityEvent;
+use pocketmine\event\inventory\CraftItemEvent;
+use pocketmine\event\inventory\FurnaceBurnEvent;
+use pocketmine\event\level\LevelLoadEvent;
+use pocketmine\event\player\PlayerBedEnterEvent;
+use pocketmine\event\player\PlayerChangeSkinEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerToggleFlightEvent;
+use pocketmine\event\player\PlayerToggleSneakEvent;
+use pocketmine\event\player\PlayerToggleSprintEvent;
+use pocketmine\event\server\LowMemoryEvent;
+
 class EventTriggerManager extends TriggerManager {
 
     /** @var array */
     private $events = [ // TODO: 名前
-        "PlayerChatEvent" => "",
-        "PlayerCommandPreprocessEvent" => "",
-        "PlayerInteractEvent" => "",
-        "PlayerJoinEvent" => "",
-        "PlayerQuitEvent" => "",
-        "BlockBreakEvent" => "",
-        "BlockPlaceEvent" => "",
-        "EntityDamageEvent" => "",
-        "EntityAttackEvent" => "",
-        "PlayerToggleFlightEvent" => "",
-        "PlayerDeathEvent" => "",
-        "EntityLevelChangeEvent" => "",
-        "CraftItemEvent" => "",
-        "PlayerDropItemEvent" => "",
+        "PlayerChatEvent" => [PlayerChatEvent::class, ""],
+        "PlayerCommandPreprocessEvent" => [PlayerCommandPreprocessEvent::class, ""],
+        "PlayerInteractEvent" => [PlayerInteractEvent::class, ""],
+        "PlayerJoinEvent" => [PlayerJoinEvent::class, ""],
+        "PlayerQuitEvent" => [PlayerQuitEvent::class, ""],
+        "BlockBreakEvent" => [BlockBreakEvent::class, ""],
+        "BlockPlaceEvent" => [BlockPlaceEvent::class, ""],
+        "SignChangeEvent" => [SignChangeEvent::class, ""],
+        "EntityDamageEvent" => [EntityDamageEvent::class, ""],
+        // "EntityAttackEvent" => [, ""],
+        "PlayerToggleFlightEvent" => [PlayerToggleFlightEvent::class, ""],
+        "PlayerDeathEvent" => [PlayerDeathEvent::class, ""],
+        "EntityLevelChangeEvent" => [EntityLevelChangeEvent::class, ""],
+        "CraftItemEvent" => [CraftItemEvent::class, ""],
+        "PlayerDropItemEvent" => [PlayerDropItemEvent::class, ""],
+        "FurnaceBurnEvent" => [FurnaceBurnEvent::class, ""],
+        "LevelLoadEvent" => [LevelLoadEvent::class, ""],
+        "PlayerBedEnterEvent" => [PlayerBedEnterEvent::class, ""],
+        "PlayerChangeSkinEvent" => [PlayerChangeSkinEvent::class, ""],
+        "PlayerExhaustEvent" => [PlayerExhaustEvent::class, ""],
+        "PlayerItemConsumeEvent" => [PlayerItemConsumeEvent::class, ""],
+        "PlayerMoveEvent" => [PlayerMoveEvent::class, ""],
+        "PlayerToggleSneakEvent" => [PlayerToggleSneakEvent::class, ""],
+        "PlayerToggleSprintEvent" => [PlayerToggleSprintEvent::class, ""],
+        "LowMemoryEvent" => [LowMemoryEvent::class, ""],
+        "ProjectileHitEntityEvent" => [ProjectileHitEntityEvent::class, ""],
     ];
 
     /** @var array */
@@ -51,5 +89,9 @@ class EventTriggerManager extends TriggerManager {
             $settings[$event] = in_array($event, $enables);
         }
         return $settings;
+    }
+
+    public function getEventPath(string $event): ?string {
+        return $this->getEvents[$event][0] ?? null;
     }
 }
