30ef28d8f74f29a1bdb313b29e3c266ef0d9380e
aieuo <35859665+aieuo@users.noreply.github.com>
2020-12-07 23:47:48 +0900

    add killer property to PlayerDeathEvent #31

--- src/aieuo/mineflow/trigger/event/PlayerDeathEventTrigger.php
+++ src/aieuo/mineflow/trigger/event/PlayerDeathEventTrigger.php
@@ -2,10 +2,35 @@
 
 namespace aieuo\mineflow\trigger\event;
 
+use aieuo\mineflow\variable\DefaultVariables;
+use aieuo\mineflow\variable\DummyVariable;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\Player;
 
 class PlayerDeathEventTrigger extends PlayerEventTrigger {
     public function __construct(string $subKey = "") {
         parent::__construct(PlayerDeathEvent::class, $subKey);
+    }
+
+    public function getVariables($event): array {
+        /** @var PlayerDeathEvent $event */
+        $target = $event->getPlayer();
+        $variables = DefaultVariables::getPlayerVariables($target);
+        $cause = $target->getLastDamageCause();
+        if ($cause instanceof EntityDamageByEntityEvent) {
+            $killer = $cause->getDamager();
+            if ($killer instanceof Player) {
+                $variables = array_merge($variables, DefaultVariables::getPlayerVariables($killer, "killer"));
+            }
+        }
+        return $variables;
+    }
+
+    public function getVariablesDummy(): array {
+        return [
+            "target" => new DummyVariable("target", DummyVariable::PLAYER),
+            "killer" => new DummyVariable("killer", DummyVariable::PLAYER),
+        ];
     }
 }
