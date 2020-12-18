5e26dac41617bc04692ae21b1b491050867c11a5
aieuo <35859665+aieuo@users.noreply.github.com>
2020-12-18 21:45:32 +0900

    escaping a newline

--- src/aieuo/mineflow/formAPI/element/Input.php
+++ src/aieuo/mineflow/formAPI/element/Input.php
@@ -69,7 +69,7 @@ class Input extends Element {
             "type" => $this->type,
             "text" => Language::replace($this->extraText).$this->reflectHighlight(Language::replace($this->text)),
             "placeholder" => Language::replace($this->placeholder),
-            "default" => Language::replace($this->default),
+            "default" => str_replace("\n", "\\n", Language::replace($this->default)),
             "mineflow" => $this->serializeExtraData(),
         ];
     }
