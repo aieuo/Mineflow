6b6c832700adce2bdb030a003ca0bdaf0d7dd154
aieuo <35859665+aieuo@users.noreply.github.com>
2021-03-22 15:57:30 +0900

    oops

--- src/aieuo/mineflow/variable/Variable.php
+++ src/aieuo/mineflow/variable/Variable.php
@@ -10,28 +10,29 @@ abstract class Variable {
     public const LIST = 2;
     public const MAP = 3;
     public const OBJECT = 4;
+    public const BOOLEAN = 5;
 
-    /** @var string 変数の名前 */
+    /** @var string */
     protected $name;
-    /** @var string|int|Variable[]|object 変数の値 */
+    /** @var string|int|Variable[]|object */
     protected $value;
-    /** @var int 変数の型 */
+    /** @var int */
     public $type;
 
     public static function create($value, string $name = "", int $type = self::STRING): ?self {
         $variable = null;
         switch ($type) {
             case self::STRING:
-                $variable = StringVariable::fromArray(["name" => $name, "value" => (string)$value]);
+                $variable = new StringVariable((string)$value, $name);
                 break;
             case self::NUMBER:
-                $variable = NumberVariable::fromArray(["name" => $name, "value" => (float)$value]);
+                $variable = new NumberVariable((float)$value, $name);
                 break;
             case self::LIST:
-                $variable = ListVariable::fromArray(["name" => $name, "value" => $value]);
+                $variable = new ListVariable($value, $name);
                 break;
             case self::MAP:
-                $variable = MapVariable::fromArray(["name" => $name, "value" => $value]);
+                $variable = new MapVariable($value, $name);
                 break;
             default:
                 return null;
@@ -64,7 +65,7 @@ abstract class Variable {
     }
 
     /**
-     * @return string|int|Variable[]|object
+     * @return string|int|Variable[]|object|bool
      */
     public function getValue() {
         return $this->value;
