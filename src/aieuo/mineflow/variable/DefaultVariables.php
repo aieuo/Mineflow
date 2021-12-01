81967936b07a3265bc9bd2214d96261d3ed97efd
aieuo <aieuo421@icloud.com>
2021-12-02 00:26:51 +0900

    add DefaultVariables::getHumanVariables

--- src/aieuo/mineflow/variable/DefaultVariables.php
+++ src/aieuo/mineflow/variable/DefaultVariables.php
@@ -4,10 +4,12 @@ namespace aieuo\mineflow\variable;
 
 use aieuo\mineflow\variable\object\BlockObjectVariable;
 use aieuo\mineflow\variable\object\EntityObjectVariable;
+use aieuo\mineflow\variable\object\HumanObjectVariable;
 use aieuo\mineflow\variable\object\PlayerObjectVariable;
 use aieuo\mineflow\variable\object\ServerObjectVariable;
 use pocketmine\block\Block;
 use pocketmine\entity\Entity;
+use pocketmine\entity\Human;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\tile\Sign;
@@ -31,7 +33,13 @@ class DefaultVariables {
 
     public static function getEntityVariables(Entity $target, string $name = "target"): array {
         if ($target instanceof Player) return self::getPlayerVariables($target, $name);
+        if ($target instanceof Human) return self::getHumanVariables($target, $name);
         return [$name => new EntityObjectVariable($target, $target->getNameTag())];
+    }
+
+    public static function getHumanVariables(Human $target, string $name = "target"): array {
+        if ($target instanceof Player) return self::getPlayerVariables($target, $name);
+        return [$name => new HumanObjectVariable($target, $target->getName())];
     }
 
     public static function getPlayerVariables(Player $target, string $name = "target"): array {
