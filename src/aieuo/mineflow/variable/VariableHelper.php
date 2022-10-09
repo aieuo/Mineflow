df67f210685d4fadebb2d04716163f793ccf0a5d
aieuo <35859665+aieuo@users.noreply.github.com>
2022-10-09 14:26:02 +0900

    [VariableHelper::isSimpleVariableString] allow dots

--- src/aieuo/mineflow/variable/VariableHelper.php
+++ src/aieuo/mineflow/variable/VariableHelper.php
@@ -396,7 +396,7 @@ class VariableHelper {
     }
 
     public function isSimpleVariableString(string $variable): bool {
-        return (bool)preg_match("/^{[^{}\[\].]+}$/u", $variable);
+        return (bool)preg_match("/^{[^{}\[\]]+}$/u", $variable);
     }
 
     public function isVariableString(string $variable): bool {
