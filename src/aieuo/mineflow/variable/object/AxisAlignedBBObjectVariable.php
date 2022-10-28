1ef6376b246b7c2229de3c6660233721636a7fd8
aieuo <35859665+aieuo@users.noreply.github.com>
2022-10-28 20:18:24 +0900

    Vector3 and AABB variables now support calculations

--- src/aieuo/mineflow/variable/object/AxisAlignedBBObjectVariable.php
+++ src/aieuo/mineflow/variable/object/AxisAlignedBBObjectVariable.php
@@ -2,6 +2,7 @@
 
 namespace aieuo\mineflow\variable\object;
 
+use aieuo\mineflow\exception\UnsupportedCalculationException;
 use aieuo\mineflow\variable\DummyVariable;
 use aieuo\mineflow\variable\NumberVariable;
 use aieuo\mineflow\variable\ObjectVariable;
@@ -9,6 +10,8 @@ use aieuo\mineflow\variable\Variable;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Vector3;
 use function array_merge;
+use function max;
+use function min;
 
 class AxisAlignedBBObjectVariable extends ObjectVariable {
 
@@ -49,8 +52,99 @@ class AxisAlignedBBObjectVariable extends ObjectVariable {
         ]);
     }
 
+    public function add($target): AxisAlignedBBObjectVariable {
+        $aabb = $this->getAxisAlignedBB();
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return new AxisAlignedBBObjectVariable($aabb->offsetCopy($value, $value, $value));
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            $vector3 = $target->getVector3();
+            return new AxisAlignedBBObjectVariable($aabb->offsetCopy($vector3->x, $vector3->y, $vector3->z));
+        }
+        if ($target instanceof AxisAlignedBBObjectVariable) {
+            $target = $target->getAxisAlignedBB();
+            return self::fromVector3(
+                new Vector3($aabb->minX + $target->minX, $aabb->minY + $target->minY, $aabb->minZ + $target->minZ),
+                new Vector3($aabb->maxX + $target->maxX, $aabb->maxY + $target->maxY, $aabb->maxZ + $target->maxZ),
+            );
+        }
+
+        throw new UnsupportedCalculationException();
+    }
+
+    public function sub($target): AxisAlignedBBObjectVariable {
+        $aabb = $this->getAxisAlignedBB();
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return new AxisAlignedBBObjectVariable($aabb->offsetCopy(-$value, -$value, -$value));
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            $vector3 = $target->getVector3();
+            return new AxisAlignedBBObjectVariable($aabb->offsetCopy(-$vector3->x, -$vector3->y, -$vector3->z));
+        }
+        if ($target instanceof AxisAlignedBBObjectVariable) {
+            $target = $target->getAxisAlignedBB();
+            return self::fromVector3(
+                new Vector3($aabb->minX - $target->minX, $aabb->minY - $target->minY, $aabb->minZ - $target->minZ),
+                new Vector3($aabb->maxX - $target->maxX, $aabb->maxY - $target->maxY, $aabb->maxZ - $target->maxZ),
+            );
+        }
+
+        throw new UnsupportedCalculationException();
+    }
+
+    public function mul($target): AxisAlignedBBObjectVariable {
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return $this->extended($value, $value, $value, $value, $value, $value);
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            $vector3 = $target->getVector3();
+            return $this->extended($vector3->x, $vector3->y, $vector3->z, $vector3->x, $vector3->y, $vector3->z);
+        }
+        if ($target instanceof AxisAlignedBBObjectVariable) {
+            $target = $target->getAxisAlignedBB();
+            return $this->extended($target->minX, $target->minY, $target->minZ, $target->maxX, $target->maxY, $target->maxZ);
+        }
+
+        throw new UnsupportedCalculationException();
+    }
+
+    public function div($target): AxisAlignedBBObjectVariable {
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return $this->extended(-$value, -$value, -$value, -$value, -$value, -$value);
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            $vector3 = $target->getVector3();
+            return $this->extended(-$vector3->x, -$vector3->y, -$vector3->z, -$vector3->x, -$vector3->y, -$vector3->z);
+        }
+        if ($target instanceof AxisAlignedBBObjectVariable) {
+            $target = $target->getAxisAlignedBB();
+            return $this->extended(-$target->minX, -$target->minY, -$target->minZ, -$target->maxX, -$target->maxY, -$target->maxZ);
+        }
+
+        throw new UnsupportedCalculationException();
+    }
+
     public function __toString(): string {
         return (string)$this->getAxisAlignedBB();
+    }
+
+    private function extended(float $minX, float $minY, float $minZ, float $maxX, float $maxY, float $maxZ): self {
+        $aabb = $this->getAxisAlignedBB();
+        return self::fromFloat(
+            $aabb->minX - $minX, $aabb->minY - $minY, $aabb->minZ - $minZ,
+            $aabb->maxX + $maxX, $aabb->maxY + $maxY, $aabb->maxZ + $maxZ,
+        );
+    }
+
+    public static function fromFloat(float $x1, float $y1, float $z1, float $x2, float $y2, float $z2): AxisAlignedBBObjectVariable {
+        return new AxisAlignedBBObjectVariable(new AxisAlignedBB(
+            min($x1, $x2), min($y1, $y2), min($z1, $z2),
+            max($x1, $x2), max($y1, $y2), max($z1, $z2),
+        ));
     }
 
 }
--- src/aieuo/mineflow/variable/object/Vector3ObjectVariable.php
+++ src/aieuo/mineflow/variable/object/Vector3ObjectVariable.php
@@ -2,6 +2,7 @@
 
 namespace aieuo\mineflow\variable\object;
 
+use aieuo\mineflow\exception\UnsupportedCalculationException;
 use aieuo\mineflow\variable\DummyVariable;
 use aieuo\mineflow\variable\NumberVariable;
 use aieuo\mineflow\variable\ObjectVariable;
@@ -50,6 +51,60 @@ class Vector3ObjectVariable extends ObjectVariable {
             "west" => new DummyVariable(DummyVariable::VECTOR3),
             "east" => new DummyVariable(DummyVariable::VECTOR3),
         ]);
+    }
+
+    public function add($target): Vector3ObjectVariable {
+        $vector3 = $this->getVector3();
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return new Vector3ObjectVariable($vector3->add($value, $value, $value));
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            return new Vector3ObjectVariable($vector3->addVector($target->getValue()));
+        }
+
+        throw new UnsupportedCalculationException();
+    }
+
+    public function sub($target): Vector3ObjectVariable {
+        $vector3 = $this->getVector3();
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return new Vector3ObjectVariable($vector3->subtract($value, $value, $value));
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            return new Vector3ObjectVariable($vector3->subtractVector($target->getValue()));
+        }
+
+        throw new UnsupportedCalculationException();
+    }
+
+    public function mul($target): Vector3ObjectVariable {
+        $vector3 = $this->getVector3();
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return new Vector3ObjectVariable(new Vector3($vector3->x * $value, $vector3->y * $value, $vector3->z * $value));
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            $value = $target->getValue();
+            return new Vector3ObjectVariable(new Vector3($vector3->x * $value->x, $vector3->y * $value->y, $vector3->z * $value->z));
+        }
+
+        throw new UnsupportedCalculationException();
+    }
+
+    public function div($target): Vector3ObjectVariable {
+        $vector3 = $this->getVector3();
+        if ($target instanceof NumberVariable) {
+            $value = $target->getValue();
+            return new Vector3ObjectVariable(new Vector3($vector3->x / $value, $vector3->y / $value, $vector3->z / $value));
+        }
+        if ($target instanceof Vector3ObjectVariable) {
+            $value = $target->getValue();
+            return new Vector3ObjectVariable(new Vector3($vector3->x / $value->x, $vector3->y / $value->y, $vector3->z / $value->z));
+        }
+
+        throw new UnsupportedCalculationException();
     }
 
     public function __toString(): string {
