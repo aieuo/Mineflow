99c92258763250651eb04b6e1a146294ea2c67a3
aieuo <35859665+aieuo@users.noreply.github.com>
2023-06-24 13:03:03 +0900

    Use PositionPlaceholder instead of PositionFlowItemTrait

--- src/aieuo/mineflow/flowItem/condition/entity/InArea.php
+++ src/aieuo/mineflow/flowItem/condition/entity/InArea.php
@@ -7,29 +7,30 @@ namespace aieuo\mineflow\flowItem\condition\entity;
 use aieuo\mineflow\flowItem\base\ConditionNameWithMineflowLanguage;
 use aieuo\mineflow\flowItem\base\EntityFlowItem;
 use aieuo\mineflow\flowItem\base\EntityFlowItemTrait;
-use aieuo\mineflow\flowItem\base\PositionFlowItem;
-use aieuo\mineflow\flowItem\base\PositionFlowItemTrait;
 use aieuo\mineflow\flowItem\condition\Condition;
 use aieuo\mineflow\flowItem\FlowItem;
 use aieuo\mineflow\flowItem\FlowItemCategory;
 use aieuo\mineflow\flowItem\FlowItemExecutor;
 use aieuo\mineflow\flowItem\form\HasSimpleEditForm;
 use aieuo\mineflow\flowItem\form\SimpleEditFormBuilder;
+use aieuo\mineflow\flowItem\placeholder\PositionPlaceholder;
 use aieuo\mineflow\formAPI\element\mineflow\EntityVariableDropdown;
-use aieuo\mineflow\formAPI\element\mineflow\PositionVariableDropdown;
 use SOFe\AwaitGenerator\Await;
 
-class InArea extends FlowItem implements Condition, EntityFlowItem, PositionFlowItem {
-    use EntityFlowItemTrait, PositionFlowItemTrait;
+class InArea extends FlowItem implements Condition, EntityFlowItem {
+    use EntityFlowItemTrait;
     use ConditionNameWithMineflowLanguage;
     use HasSimpleEditForm;
+
+    private PositionPlaceholder $position1;
+    private PositionPlaceholder $position2;
 
     public function __construct(string $entity = "", string $pos1 = "", string $pos2 = "") {
         parent::__construct(self::IN_AREA, FlowItemCategory::ENTITY);
 
         $this->setEntityVariableName($entity);
-        $this->setPositionVariableName($pos1, "pos1");
-        $this->setPositionVariableName($pos2, "pos2");
+        $this->position1 = new PositionPlaceholder("pos1", $pos1, "@condition.inArea.form.pos1");
+        $this->position2 = new PositionPlaceholder("pos2", $pos2, "@condition.inArea.form.pos2");
     }
 
     public function getDetailDefaultReplaces(): array {
@@ -37,17 +38,25 @@ class InArea extends FlowItem implements Condition, EntityFlowItem, PositionFlow
     }
 
     public function getDetailReplaces(): array {
-        return [$this->getEntityVariableName(), $this->getPositionVariableName("pos1"), $this->getPositionVariableName("pos2")];
+        return [$this->getEntityVariableName(), $this->position1->get(), $this->position2->get()];
     }
 
     public function isDataValid(): bool {
-        return $this->getEntityVariableName() !== "" and $this->getPositionVariableName("pos1") !== "" and $this->getPositionVariableName("pos2") !== "";
+        return $this->getEntityVariableName() !== "" and $this->position1->isNotEmpty() and $this->position2->isNotEmpty();
+    }
+
+    public function getPosition1(): PositionPlaceholder {
+        return $this->position1;
+    }
+
+    public function getPosition2(): PositionPlaceholder {
+        return $this->position2;
     }
 
     protected function onExecute(FlowItemExecutor $source): \Generator {
         $entity = $this->getOnlineEntity($source);
-        $pos1 = $this->getPosition($source, "pos1");
-        $pos2 = $this->getPosition($source, "pos2");
+        $pos1 = $this->position1->getPosition($source);
+        $pos2 = $this->position2->getPosition($source);
         $pos = $entity->getLocation()->floor();
 
         yield Await::ALL;
@@ -59,18 +68,18 @@ class InArea extends FlowItem implements Condition, EntityFlowItem, PositionFlow
     public function buildEditForm(SimpleEditFormBuilder $builder, array $variables): void {
         $builder->elements([
             new EntityVariableDropdown($variables, $this->getEntityVariableName()),
-            new PositionVariableDropdown($variables, $this->getPositionVariableName("pos1"), "@condition.inArea.form.pos1"),
-            new PositionVariableDropdown($variables, $this->getPositionVariableName("pos2"), "@condition.inArea.form.pos2"),
+            $this->position1->createFormElement($variables),
+            $this->position2->createFormElement($variables),
         ]);
     }
 
     public function loadSaveData(array $content): void {
         $this->setEntityVariableName($content[0]);
-        $this->setPositionVariableName($content[1], "pos1");
-        $this->setPositionVariableName($content[2], "pos2");
+        $this->position1->set($content[1]);
+        $this->position2->set($content[2]);
     }
 
     public function serializeContents(): array {
-        return [$this->getEntityVariableName(), $this->getPositionVariableName("pos1"), $this->getPositionVariableName("pos2")];
+        return [$this->getEntityVariableName(), $this->position1->get(), $this->position2->get()];
     }
 }
